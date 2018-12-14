df30e1bd7014d0c8a0a7987ff0bf45e1988f9118
VirgilMa <15302010022@fudan.edu.cn>
2018-12-14 21:59:35 +0800

    update interface.md

--- front-end/project/components/task/task.js
+++ front-end/project/components/task/task.js
@@ -4,14 +4,23 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    innerText: {
+      type: String,
+      value: 'hello word',
+    },
+    pictureUrl: {
+      type: String,
+      value: 'https://pic2.zhimg.com/v2-688c1cdaf74350d8c6aea7145aa1084c_1200x500.jpg'
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    loaded:false,
+    title:null,
+    username:null,
   },
 
   /**
