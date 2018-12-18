8797ddafe8fee0aa9101781f1b25d67f019abb0e
VirgilMa <15302010022@fudan.edu.cn>
2018-12-18 12:31:01 +0800

    朋友列表

---
+++ front-end/project/components/ftask/ftask.js
@@ -0,0 +1,43 @@
+// components/ftask/ftask.js
+
+Component({
+  properties: { // TODO: 用一个大属性来整合小属性
+    title: {
+      type: String,
+      value: '代练' // 'Loading...'
+    },
+    pictureUrl: {
+      type: String,
+      value: '/img/test/cj.jpg' // no pic
+    },
+    account: {
+      type: Number,
+      value: 10
+    },
+    username: {
+      type: String,
+      value: '无名氏'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+    click: function () {
+      wx.navigateTo({
+        url: '../task_detail/task_detail',
+        success: function (res) { },
+        fail: function (res) { },
+        complete: function (res) { },
+      })
+    }
+  },
+
+})
