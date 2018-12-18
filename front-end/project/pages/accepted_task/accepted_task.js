3309c1f022acc0c0a5e51ec3d978d39628f602d5
VirgilMa <15302010022@fudan.edu.cn>
2018-12-18 17:57:03 +0800

    关注任务美化

--- front-end/project/pages/accepted_task/accepted_task.js
+++ front-end/project/pages/accepted_task/accepted_task.js
@@ -5,11 +5,12 @@ Page({
     tasks: [{
       task_id: 1,
       title: '打败拿破仑',
-      // picture: '',
+      picture: '/img/test/cj.jpg',
       end_time: 'Dec-15-2018 15:09:00',
       sender_name: '亚历山大一世',
       sender_avatar: '/img/test/boss.png',
       status: 1,
+      left_time: '3 小时',
     }]
   },
 
