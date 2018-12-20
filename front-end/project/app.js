29bc9cc7204e974c39eeca7969402c155242bab1
fzhu1 <fzhu1@ebay.com>
2018-12-20 16:51:29 +0800

    update authorization page

--- front-end/project/app.js
+++ front-end/project/app.js
@@ -5,9 +5,13 @@ App({
     // get userinfo
     wx.getSetting({
       success: res => {
+        this.globalData.canIuse = true
         if (res.authSetting['scope.userInfo'])
           this.login()
       },
+      fail: res => {
+        this.globalData.canIuse = false
+      }
     });
   },
 
@@ -60,6 +64,7 @@ App({
   globalData: {
     userInfo: null,
     tasks: {},
+    canIuse: false
   },
 
 })
